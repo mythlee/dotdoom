0b5c3f7920770aa2d9e237c561206355187d4f84
Li Hui <mythlee@ustc.edu.cn>
2022-01-24 20:56:39 +0800

    add tex sync config

--- config-tex.el
+++ config-tex.el
@@ -2,3 +2,10 @@
 
 (add-hook 'LaTeX-mode-hook 'reftex-mode)
 (setq reftex-plug-into-AUCTeX t)
+
+(setq-default TeX-master nil)
+
+;; ##### Enable synctex correlation. From Okular just press
+;; ##### Shift + Left click to go to the good line.
+(setq TeX-source-correlate-mode t
+      TeX-source-correlate-start-server t)
