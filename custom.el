cde5b2c57e0caa705386276ff4aa6325b5574c2d
Li Hui <mythlee@ustc.edu.cn>
2022-07-29 10:59:25 +0800

    nuc2 setup

--- custom.el
+++ custom.el
@@ -3,7 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(warning-suppress-log-types '((org-element-cache))))
+ '(custom-safe-themes
+   '("a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "333958c446e920f5c350c4b4016908c130c3b46d590af91e1e7e2a0611f1e8c5" "5f19cb23200e0ac301d42b880641128833067d341d22344806cdad48e6ec62f6" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "f7fed1aadf1967523c120c4c82ea48442a51ac65074ba544a5aefc5af490893b" "c2aeb1bd4aa80f1e4f95746bda040aafb78b1808de07d340007ba898efa484f5" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" default))
+ '(warning-suppress-log-types '((org-element-cache)))
+ '(warning-suppress-types '(((python python-shell-completion-native-turn-on-maybe)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
